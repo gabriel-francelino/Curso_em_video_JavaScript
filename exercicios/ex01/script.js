07efdade77c7906cba8b6cf3f3be0d95ef9bd60e
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-07-25 21:45:01 -0300

    Pegando a hora do computador

--- exercicios/ex01/script.js
+++ exercicios/ex01/script.js
@@ -2,8 +2,7 @@ function carregar() {
     var msg = window.document.getElementById('msg');
     var img = window.document.getElementById('imagem');
     var data = new Date();
-    //var hora = data.getHours();
-    var hora = 16
+    var hora = data.getHours();
     msg.innerHTML = `Horário: ${hora} horas.`;
     if (hora>=0 && hora<12) {
         img.src = 'img/manha.jpg';
